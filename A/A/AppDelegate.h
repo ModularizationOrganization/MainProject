f589b8c9d9f50d170e816e1aa824c1cd6f98cd6b
Darkrai <516998804@qq.com>
2017-03-24 15:10:17 +0800

    originCommit

---
+++ A/A/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  A
+//
+//  Created by Darkrai on 2017/3/24.
+//  Copyright © 2017年 Minos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ A/A/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  A
+//
+//  Created by Darkrai on 2017/3/24.
+//  Copyright © 2017年 Minos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
